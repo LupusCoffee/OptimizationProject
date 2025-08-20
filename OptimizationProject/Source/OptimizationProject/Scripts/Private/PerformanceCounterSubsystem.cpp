ee44f5aa737e72b3a6e29cc39f7777a928ffe8e1
LupusCoffee <96193514+LupusCoffee@users.noreply.github.com>
2025-08-20 18:48:40 +0200

    Performance Checkpoint: Optimization, Iteration 1: 10 --> 5 pieces in geo coll

--- OptimizationProject/Source/OptimizationProject/Scripts/Private/PerformanceCounterSubsystem.cpp
+++ OptimizationProject/Source/OptimizationProject/Scripts/Private/PerformanceCounterSubsystem.cpp
@@ -16,8 +16,14 @@ void UPerformanceCounterSubsystem::AddCupStaticMeshes(int Value)
 	TRACE_COUNTER_SET(CupStaticMeshes, CupStaticMeshes);
 #endif
 
-	if (CupStaticMeshes > 1500)
-		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, "Stop Debugging");
+	if (CupStaticMeshes > 500 && CupStaticMeshes < 505)
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Yellow, "500 CupStaticMeshes");
+
+	if (CupStaticMeshes > 1000 && CupStaticMeshes < 1005)
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Yellow, "1000 CupStaticMeshes");
+
+	if (CupStaticMeshes > 1500 && CupStaticMeshes < 1505)
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Yellow, "1500 CupStaticMeshes");
 }
 
 void UPerformanceCounterSubsystem::RemoveCupStaticMeshes(int Value)
@@ -37,8 +43,14 @@ void UPerformanceCounterSubsystem::AddUnfracturedCupGeoColls(int Value)
 	TRACE_COUNTER_SET(UnfracturedCupGeoColls, UnfracturedCupGeoColls);
 #endif
 
-	if (UnfracturedCupGeoColls > 1500)
-		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, "Stop Debugging");
+	if (UnfracturedCupGeoColls > 500 && UnfracturedCupGeoColls < 505)
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Orange, "500 UnfracturedCupGeoColls");
+
+	if (UnfracturedCupGeoColls > 1000 && UnfracturedCupGeoColls < 1005)
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Orange, "1000 UnfracturedCupGeoColls");
+
+	if (UnfracturedCupGeoColls > 1500 && UnfracturedCupGeoColls < 1505)
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Orange, "1500 UnfracturedCupGeoColls");
 }
 
 void UPerformanceCounterSubsystem::RemoveUnfracturedCupGeoColls(int Value)
@@ -58,8 +70,14 @@ void UPerformanceCounterSubsystem::AddFracturedCupGeoColls(int Value)
 	TRACE_COUNTER_SET(FracturedCupGeoColls, FracturedCupGeoColls);
 #endif
 
-	if (FracturedCupGeoColls > 1500)
-		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, "Stop Debugging");
+	if (FracturedCupGeoColls > 500 && FracturedCupGeoColls < 505)
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, "500 FracturedCupGeoColls");
+
+	if (FracturedCupGeoColls > 1000 && FracturedCupGeoColls < 1005)
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, "1000 FracturedCupGeoColls");
+
+	if (FracturedCupGeoColls > 1500 && FracturedCupGeoColls < 1505)
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, "1500 FracturedCupGeoColls");
 }
 
 void UPerformanceCounterSubsystem::RemoveFracturedCupGeoColls(int Value)
